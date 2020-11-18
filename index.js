a1808b3fb4177af5db29184e461e344d52bd4f76
Madison Bertis <madisonbertis@gmail.com>
2020-11-18 14:27:32 -0800

    added routes for all endpoints

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express"),
-  morgan = require("morgan");
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  uuid = require('uuid');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 let topMovies = [
   {
@@ -48,8 +52,49 @@ let topMovies = [
 
 app.use(morgan("common"));
 
+// Gets the list of data about all movies
 app.get("/movies", (req, res) => {
   res.json(topMovies);
+});
+
+// Gets the data about a single movie, by title
+app.get("/movies/:title", (req, res) => {
+  res.send("Successful GET request returning data on entered movie");
+});
+
+//Gets information about a genre by name
+app.get("/movies/:genre", (req, res) => {
+  res.send("Successful GET request returning data on entered genre");
+});
+
+//Gets information about a director by name
+app.get("/movies/:director", (req, res) => {
+  res.send("Successful GET request returning data on entered director");
+});
+
+//Allows new users to register
+app.post("/users", (req, res) => {
+  res.send("New user created successfully");
+});
+
+//Allows user to update username
+app.put("/users/:email/:username", (req, res) => {
+  res.send("Username updated successfully");
+});
+
+//Add a movie to list of favorites
+app.post("/movies", (req, res) => {
+  res.send("Movie added to list of favorites");
+});
+
+//Remove a movie from list of favorites
+app.delete("/movies/:title", (req, res) => {
+  res.send("Movie was removed from favorites list");
+});
+
+//Allow users to deregister
+app.delete("/users/:email", (req, res) => {
+  res.send("User has successfully deregistered");
 });
 
 app.get("/", (req, res) => {
