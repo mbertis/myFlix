fe696fa544fb2c20733115d6a5acf93a96ad040f
Madison Bertis <madisonbertis@gmail.com>
2020-11-30 15:28:53 -0800

    formatted with Prettier

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ const app = express(),
   Directors = Models.Director,
   Genres = Models.Genre;
 
-
-mongoose.connect("mongodb://localhost:27017/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.json());
 
@@ -64,49 +66,49 @@ app.use(morgan("common"));
 // Gets the list of data about all movies
 app.get("/movies", (req, res) => {
   Movies.find()
-  .then((movies) => {
-    res.status(201).json(movies);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 // Gets the data about a single movie, by title
 app.get("/movies/:Title", (req, res) => {
-  Movies.findOne({Title: req.params.Title})
-  .then((movie) => {
-    res.status(201).json(movie);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.status(201).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Gets information about a genre by name
 app.get("/genres/:Name", (req, res) => {
-  Genres.findOne({Name: req.params.Name})
-  .then((genre) => {
-    res.status(201).json(genre);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+  Genres.findOne({ Name: req.params.Name })
+    .then((genre) => {
+      res.status(201).json(genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Gets information about a director by name
 app.get("/directors/:Name", (req, res) => {
-  Directors.findOne({Name: req.params.Name})
-  .then((director) => {
-    res.json(director);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.json(500).send("Error: " + err);
-  });
+  Directors.findOne({ Name: req.params.Name })
+    .then((director) => {
+      res.json(director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.json(500).send("Error: " + err);
+    });
 });
 
 //Allows new users to register
@@ -119,53 +121,54 @@ app.get("/directors/:Name", (req, res) => {
   Birthday: Date
 }*/
 app.post("/users", (req, res) => {
-  Users.findOne({Username: req.body.Username})
-  .then((user) => {
-    if(user) {
-      return res.status(400).send(req.body.Username + " already exists");
-    } else {
-      Users
-      .create({
-        Username: req.body.Username,
-        Password: req.body.Password,
-        Email: req.body.Email,
-        Birthday: req.body.Birthday
-      })
-      .then((user) => {res.status(201).json(user)})
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      })
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-    res.status(500).send("Error: " + error);
-  });
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + " already exists");
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 });
 
 //Get information about ALL users
 app.get("/users", (req, res) => {
   Users.find()
-  .then((users) => {
-    res.status(201).json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Get a user by username
 app.get("/users/:Username", (req, res) => {
-  Users.findOne({Username: req.params.Username})
-  .then((user) => {
-    res.json(user);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 //Allows user to update their info by username
@@ -177,72 +180,80 @@ app.get("/users/:Username", (req, res) => {
   Birthday: Date
 }*/
 app.put("/users/:Username", (req, res) => {
-  Users.findOneAndUpdate({Username: req.params.Username}, {$set: 
-  {
-    Username: req.body.Username,
-    Password: req.body.Password,
-    Email: req.body.Email,
-    Birthday: req.body.Birthday
-  }
-  },
-  {new: true}, //This makes sure the updated document is returned
-  (err, updatedUser) => {
-    if(err) {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    } else {
-      res.json(updatedUser);
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $set: {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday,
+      },
+    },
+    { new: true }, //This makes sure the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
     }
-  });
+  );
 });
 
 //Add a movie to list of favorites
 app.post("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.findOneAndUpdate({Username: req.params.Username}, {
-    $push: { FavoriteMovies: req.params.MovieID }
-  },
-  {new: true}, //This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    } else {
-      res.json(updatedUser);
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $push: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, //This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
     }
-  });
+  );
 });
 
 //Remove a movie from list of favorites
 app.delete("/users/:Username/Movies/:MovieID", (req, res) => {
-  Users.findOneAndUpdate({Username: req.params.Username}, {
-    $pull: { FavoriteMovies: req.params.MovieID }
-  },
-  {new: true}, //This line makes sure that the updated document is returned
-  (err, updatedUser) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    } else {
-      res.json(updatedUser);
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true }, //This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      } else {
+        res.json(updatedUser);
+      }
     }
-  });
+  );
 });
-
 
 //Allow users to deregister, by username
 app.delete("/users/:Username", (req, res) => {
-  Users.findOneAndRemove({Username: req.params.Username})
-  .then((user) => {
-    if (!user) {
-      res.status(400).send(req.params.Username + " was not found");
-    } else {
-      res.status(200).send(req.params.Username + " was successfully deleted");
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Error: " + err);
-  });
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was successfully deleted");
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
 });
 
 app.get("/", (req, res) => {
