804aa2723379c1b89c8b974da7d7b35e88acf0da
Madison Bertis <madisonbertis@gmail.com>
2020-11-30 13:00:30 -0800

    added GET request for user by username

--- index.js
+++ index.js
@@ -125,6 +125,18 @@ app.get("/users", (req, res) => {
   });
 });
 
+//Get a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({Username: req.params.Username})
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
 //Allows user to update username
 app.put("/users/:email/:username", (req, res) => {
   res.send(
