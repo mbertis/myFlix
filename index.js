01374a0526530f5479b6c3da5a55c5a0045e4ec8
Madison Bertis <madisonbertis@gmail.com>
2020-12-02 12:35:37 -0800

    installed cors and required cors in index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require("express"),
   uuid = require("uuid"),
   mongoose = require("mongoose"),
   Models = require("./models.js"),
-  passport = require("passport");
+  passport = require("passport"),
+  cors = require("cors");
 
 require("./passport");
 
@@ -22,6 +23,19 @@ mongoose.connect("mongodb://localhost:27017/myFlixDB", {
 app.use(bodyParser.json());
 
 let auth = require("./auth")(app); //Passes auth.js into this file, also allows Express to be available in auth.js
+
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn't found on the list of allowed origins
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 let topMovies = [
   {
