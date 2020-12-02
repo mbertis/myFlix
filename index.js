a502ed8b4a61501cb4fd046397d072033e0c055c
Madison Bertis <madisonbertis@gmail.com>
2020-12-02 14:19:16 -0800

    modified endpoint for registering new user to hash password before storing it

--- index.js
+++ index.js
@@ -157,6 +157,7 @@ app.get(
   Birthday: Date
 }*/
 app.post("/users", (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -164,7 +165,7 @@ app.post("/users", (req, res) => {
       } else {
         Users.create({
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         })
